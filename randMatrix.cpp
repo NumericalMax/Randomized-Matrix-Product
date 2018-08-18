be3fb42644ea06c983be6ddc6c9c0a517d4f58c4
Vimuth Fernando <wvf2@illinois.edu>
2018-08-18 14:48:45 -0500

    Bug in euclideanNormCols
    The indexes need to be switched around.
    
    Causes segmentation faults when using with non square matrices (ex: A: 10 x 10 and B: 10 x 20)

--- randMatrix.cpp
+++ randMatrix.cpp
@@ -131,7 +131,7 @@ namespace matrixOperations{
         
         float norm = 0.0;
         for(int i = 0; i < size_l; i++){
-            norm += array[i][index] * array[i][index];
+            norm += array[index][i] * array[index][i];
         }
         return sqrt(norm);
     }
