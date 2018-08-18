c6838f845c0a4cfb30fde1b6334e582eb8033da4
Vimuth Fernando <wvf2@illinois.edu>
2018-08-18 15:21:38 -0500

    Updated euclideanNormRows
    The indexes seem to be switched around

--- randMatrix.cpp
+++ randMatrix.cpp
@@ -112,7 +112,7 @@ namespace matrixOperations{
         
         float norm = 0.0;
         for(int i = 0; i < size_l; i++){
-            norm += A[index][i] * A[index][i];
+            norm += A[i][index] * A[i][index];
         }
         return sqrt(norm);
     }
